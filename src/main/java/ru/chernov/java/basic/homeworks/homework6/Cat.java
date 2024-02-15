90582511a412bd023a112e1d904a4f2fb68c836e
BlackHedge <maksim.blackhedge@gmail.com>
2024-02-16 00:48:22 +0300

    корректировка по комментарию

--- src/main/java/ru/chernov/java/basic/homeworks/homework6/Cat.java
+++ src/main/java/ru/chernov/java/basic/homeworks/homework6/Cat.java
@@ -3,7 +3,7 @@ package ru.chernov.java.basic.homeworks.homework6;
 public class Cat {
     private String name;
     private int appetite;
-    private boolean satiety = false;
+    private boolean satiety;
 
     public Cat(String name, int appetite) {
         this.name = name;
