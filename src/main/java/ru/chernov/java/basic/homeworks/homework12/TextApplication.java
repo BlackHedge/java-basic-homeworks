9a7e0f3b73de10f421118e68671a1a245918cc13
BlackHedge <maksim.blackhedge@gmail.com>
2024-04-09 02:44:50 +0300

    ДЗ12. корректировки по комментариям

--- src/main/java/ru/chernov/java/basic/homeworks/homework12/TextApplication.java
+++ src/main/java/ru/chernov/java/basic/homeworks/homework12/TextApplication.java
@@ -5,101 +5,104 @@ import java.util.*;
 
 public class TextApplication {
     public static void main(String[] args) throws IOException {
-        File file = new File("src");
-        System.out.println("Список txt файлов в папке: " + file.getPath());
-        List<File> listFiles = getListFiles(file, "txt");
-
+        System.out.println("Список txt файлов в папке \"src\": ");
+        List<String> listFiles = getListFiles("src", "txt");
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Укажите имя файла для редактирования (без расширения)");
-        String inputFileName = scanner.nextLine();
-
-        int tries = listFiles.size();
-        File workFile = null;
-        for (File f : listFiles) {
-            tries -= 1;
-            if (f.getName().endsWith(inputFileName + ".txt")) {
-                workFile = f;
-                readFile(f);
-                break;
+        String inputFileName = "src\\" + scanner.nextLine() + ".txt";
+        try {
+            if (searchFileInList(listFiles, inputFileName)) {
+                readFile(inputFileName);
+                boolean choice = false;
+                do {
+                    System.out.println("Добавить новые данные в файл? (Y/N)");
+                    if (yesNoChoice()) {
+                        System.out.println("Введите строку для добавления в файл");
+                        String inputData = scanner.nextLine();
+                        writeFile(inputFileName, inputData);
+                        System.out.println("Вывести новое содержимое а экран? (Y/N)");
+                        if (yesNoChoice()) {
+                            readFile(inputFileName);
+                            choice = true;
+                        } else {
+                            System.out.println("Работа с файлом завершена");
+                            return;
+                        }
+                    } else {
+                        System.out.println("Работа с файлом завершена");
+                        return;
+                    }
+                } while (choice);
             }
-            if (tries == 0) {
-                System.out.println("Файл не найден");
-                return;
-            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage() + "\nРабота программы завершена");
+            return;
         }
-
-        String choice = "N";
-        do {
-            System.out.println("Добавить новые данные в файл? (Y/N)");
-            choice = scanner.nextLine().toUpperCase();
-            if (choice.equals("Y")) {
-                System.out.println("Введите строку для добавления в файл");
-                String inputData = scanner.nextLine();
-                writeFile(workFile, inputData);
-                System.out.println("Вывести новое содержимое а экран? (Y/N)");
-                String choiceOut = scanner.nextLine().toUpperCase();
-                if (choiceOut.equals("Y")) {
-                    readFile(workFile);
-                } else {
-                    System.out.println("Работа с файлом завершена");
-                    return;
-                }
-            }
-        } while (choice.equals("Y"));
-
-        if (choice.equals("N")) {
-            System.out.println("Работа с файлом завершена");
-        }
-
     }
 
-    private static void readFile(File f) {
-        System.out.println("#Начало файла" + "\n" + "----------");
-        try (InputStreamReader inFile = new InputStreamReader(new FileInputStream(f))) {
+
+    private static void readFile(String f) throws FileNotFoundException, IOException {
+        System.out.println("#Начало файла" + "\n" + "-->");
+        File readFile = new File(f);
+        try (InputStreamReader inFile = new InputStreamReader(new BufferedInputStream(new FileInputStream(readFile)))) {
             int n = inFile.read();
             while (n != -1) {
                 System.out.print((char) n);
                 n = inFile.read();
             }
+            System.out.println("\n<--" + "\n#Конец файла");
         } catch (IOException e) {
             e.printStackTrace();
-            return;
-        } catch (NullPointerException e) {
-            System.out.println("Файл не определен, вывод на экран не возможен");
-            return;
         }
-        System.out.println("\n--------" + "\n#Конец файла");
     }
 
-    private static void writeFile(File f, String data) {
-        try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(f))) {
+    private static void writeFile(String f, String data) throws IOException {
+        File writtenFile = new File(f);
+        try (OutputStreamWriter writer = new OutputStreamWriter(new BufferedOutputStream(new FileOutputStream(writtenFile)))) {
             writer.write(data);
             writer.close();
             System.out.println("Данные записаны в файл");
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (NullPointerException e) {
-            System.out.println("Файл не определен, запись не выполнена");
         }
     }
 
-    public static List<File> getListFiles(File file, String ext) {
-        List<File> listFiles = new ArrayList<>();
+    public static List<String> getListFiles(String path, String ext) {
+        List<String> listFiles = new ArrayList<>();
         int cnt = 0;
-        for (File f : file.listFiles()) {
+        for (File f : new File(path).listFiles()) {
             if (f.isDirectory()) {
-                getListFiles(f, ext);
+                getListFiles(f.getPath(), ext);
             }
             if (f.isFile() && f.getName().endsWith("." + ext)) {
                 cnt++;
                 if (cnt == 1) {
-                    System.out.println("folder:: " + f.getParent());
+                    System.out.println("folder:: " + f.getParent() + "\\");
                 }
-                listFiles.add(f);
+                listFiles.add(f.getPath());
                 System.out.println("-" + f.getName());
             }
         }
         return listFiles;
+    }
+
+    public static boolean searchFileInList(List<String> listFiles, String fileName) throws FileNotFoundException {
+        int tries = listFiles.size();
+        for (String f : listFiles) {
+            tries -= 1;
+            if (f.equals(fileName)) {
+                return true;
+            }
+        }
+        if (tries == 0) {
+            throw new FileNotFoundException("Файл не найден");
+        }
+        return false;
+    }
+
+    public static boolean yesNoChoice() {
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.nextLine().toUpperCase();
+        return choice.equals("Y");
     }
 }
